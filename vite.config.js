8a06bf80f34f83a97ac381c3515b38dcfbb45977
Adam <adamhinckley@mac.com>
2026-03-16 17:01:15 -0500

    refactor: remove unused JSDOM import and postProcess function from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 import prerender from "@prerenderer/rollup-plugin";
 import JSDOMRenderer from "@prerenderer/renderer-jsdom";
-import { JSDOM } from "jsdom";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,17 +15,6 @@ export default defineConfig({
           renderer: new JSDOMRenderer(),
           rendererOptions: {
             renderAfterTime: 2000,
-          },
-          postProcess(renderedRoute) {
-            // Ensure the prerendered HTML is minified-friendly
-            const dom = new JSDOM(renderedRoute.html);
-            const app = dom.window.document.querySelector("#app");
-            if (app) {
-              const appContent = app.innerHTML;
-              app.innerHTML = appContent;
-            }
-            renderedRoute.html = dom.serialize();
-            return renderedRoute;
           },
         }),
       ],
